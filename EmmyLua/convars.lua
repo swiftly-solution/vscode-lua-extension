cb71cde56f36f0580e00a798fbe14c738c7bb74c
skuzzis <andreieurt@gmail.com>
2024-09-15 20:17:16 +0300

    update(generator): Data

--- EmmyLua/convars.lua
+++ EmmyLua/convars.lua
@@ -31,3 +31,36 @@ function convar:GetType(name) end
 --- @param value string
 --- @return nil
 function convar:Set(name, value) end
+
+--- Checks if a convar exists.
+--- @param name string
+--- @return bool
+function convar:Exists(name) end
+
+--- Checks if a fake convar exists.
+--- @param name string
+--- @return bool
+function convar:ExistsFake(name) end
+
+--- Adds the flags to the convar.
+--- @param name string
+--- @param flags number
+--- @return nil
+function convar:AddFlags(name, flags) end
+
+--- Removes the flags from the convar.
+--- @param name string
+--- @param flags number
+--- @return nil
+function convar:RemoveFlags(name, flags) end
+
+--- Returns the convar flags.
+--- @param name string
+--- @return number
+function convar:GetFlags(name) end
+
+--- Checks if a convar has the flags.
+--- @param name string
+--- @param flags number
+--- @return bool
+function convar:HasFlags(name, flags) end
